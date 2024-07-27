c507f01a2fe84ea83a1344caa3ab216ab13c0941
noahk0 <noah.kim@mail.utoronto.ca>
2024-07-28 05:53:35 +0900

    Update hard Binary Tree Maximum Path Sum O(n).py

--- hard Binary Tree Maximum Path Sum O(n).py
+++ hard Binary Tree Maximum Path Sum O(n).py
@@ -1,12 +1,12 @@
 def maxPathSum(self, root: Optional[TreeNode]) -> int:
     def dfs(node):
         left, right = max(dfs(node.left), 0) if node.left else 0, max(dfs(node.right), 0) if node.right else 0
-        total[0] = max(total[0], node.val + left + right)
+        self.total = max(self.total, node.val + left + right)
 
         return node.val + max(left, right)
 
-    total = [root.val]
+    self.total = root.val
 
     dfs(root)
 
-    return total[0]
+    return self.total
