68fe970bb358ce8fdd0f70d931eaab4dd413334f
noahk0 <noah.kim@mail.utoronto.ca>
2024-07-28 06:11:17 +0900

    Create hard Serialize and Deserialize Binary Tree O(n).py

---
+++ hard Serialize and Deserialize Binary Tree O(n).py
@@ -0,0 +1,31 @@
+class Codec:
+
+    def serialize(self, root):
+        serial = []
+
+        def dfs(node):
+            if node:
+                serial.append(str(node.val))
+                dfs(node.left)
+                dfs(node.right)
+            else:
+                serial.append('!')
+
+        dfs(root)
+        
+        return ' '.join(serial)
+        
+
+    def deserialize(self, data):
+        self.i, data = 0, iter(data.split())
+
+        def dfs():
+            val = next(data)
+
+            if val != '!':
+                root = TreeNode(val)
+                root.left, root.right = dfs(), dfs()
+
+                return root
+
+        return dfs()
