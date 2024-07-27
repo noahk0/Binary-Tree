2bc5e0f6a4cded8460df71d77e9cdb90bfedea9c
noahk0 <noah.kim@mail.utoronto.ca>
2024-07-27 15:31:31 +0900

    Update !easy Invert Binary Tree O(n).py

--- !easy Invert Binary Tree O(n).py
+++ !easy Invert Binary Tree O(n).py
@@ -1,10 +1,8 @@
 def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-    if not root:
-        return None
+    if root:
+        root.left, root.right = root.right, root.left
 
-    root.left, root.right = root.right, root.left
-
-    self.invertTree(root.left)
-    self.invertTree(root.right)
+        self.invertTree(root.left)
+        self.invertTree(root.right)
 
     return root
